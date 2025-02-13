f2b2494ee8e2c993a86188e1d50ac84377ba1061
debian <debian@vbox.myguest.virtualbox.org>
2025-02-13 18:02:30 -0300

    inicio do layout

--- src/main.py
+++ src/main.py
@@ -3,7 +3,10 @@ import customtkinter as ctk
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
-        self.geometry("500x500")
+        self.geometry("1200x600")
+        # Widgets
+        self.accounts = ctk.CTkButton(self, text="Contas")
+        self.accounts.grid(column=0, row=0, padx=20, pady=20)
 
 if __name__ == "__main__":
     app = App()
